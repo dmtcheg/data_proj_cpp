bc3930317002674747f1bf663b426115211a8daf
dmtcheg <pigrastrigo@gmail.com>
2022-04-26 09:54:46 +0300

    ctor & cout

--- employee.cpp
+++ employee.cpp
@@ -1,14 +1,30 @@
 #include "employee.h"
 #include <string>
 #include <vector>
+#include "utils.cpp"
 
 using namespace std;
 
 class Employee {
 public:
-    Employee(string line){
+    Employee(string line) {
+        auto fields = readCSVRow(line);
+        name = fields[0];
+        age = stoi(fields[1]);
+        department = fields[2];
+        position = fields[3];
+        boss_name = fields[4];
+        for (int i = 5; i < size(fields); ++i) {
+            working_days.push_back(fields[i]);
+        }
+    }
+
+    static Employee *make_employee(string line) {
 
     }
+
+    friend ostream &operator<<(ostream &os, const Employee &e);
+
 private:
     string name;
     int age;
@@ -17,3 +33,8 @@ private:
     string boss_name;
     vector<string> working_days;
 };
+
+ostream &operator<<(ostream &os, const Employee &e) {
+    os << "Name: " << e.name << ", age" << e.age << ", department: " << e.department << ", boss: " << e.boss_name;
+    return os;
+}
---
+++ utils.cpp
@@ -0,0 +1,77 @@
+#include <istream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class CSVState {
+    UnquotedField,
+    QuotedField,
+    QuotedQuote
+};
+
+vector<string> readCSVRow(const string &row) {
+    CSVState state = CSVState::UnquotedField;
+    vector<string> fields{""};
+    size_t i = 0; // index of the current field
+    for (char c: row) {
+        switch (state) {
+            case CSVState::UnquotedField:
+                switch (c) {
+                    case '\t': // end of field
+                        fields.push_back("");
+                        i++;
+                        break;
+                    case '"':
+                        state = CSVState::QuotedField;
+                        break;
+                    default:
+                        fields[i].push_back(c);
+                        break;
+                }
+                break;
+            case CSVState::QuotedField:
+                switch (c) {
+                    case '"':
+                        state = CSVState::QuotedQuote;
+                        break;
+                    default:
+                        fields[i].push_back(c);
+                        break;
+                }
+                break;
+            case CSVState::QuotedQuote:
+                switch (c) {
+                    case '\t': // , after closing quote
+                        fields.push_back("");
+                        i++;
+                        state = CSVState::UnquotedField;
+                        break;
+                    case '"': // "" -> "
+                        fields[i].push_back('"');
+                        state = CSVState::QuotedField;
+                        break;
+                    default:  // end of quote
+                        state = CSVState::UnquotedField;
+                        break;
+                }
+                break;
+        }
+    }
+    return fields;
+}
+
+/// Read CSV file, Excel dialect. Accept "quoted fields ""with quotes"""
+vector<vector<string>> readCSV(istream &in) {
+    vector<vector<string>> table;
+    string row;
+    while (!in.eof()) {
+        getline(in, row);
+        if (in.bad() || in.fail()) {
+            break;
+        }
+        auto fields = readCSVRow(row);
+        table.push_back(fields);
+    }
+    return table;
+}
