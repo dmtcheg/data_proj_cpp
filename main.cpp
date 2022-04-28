a8af04fe705745696b0de302ac1bd58cfdaa84be
dmtcheg <pigrastrigo@gmail.com>
2022-04-28 19:48:16 +0300

    add switch cases

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 #include "register_context.h"
 using namespace std;
 
+void print(vector<shared_ptr<Employee>> employess){
+    for (auto e: employess){
+        cout<<*e;
+    }
+}
+
 void ui(Register reg){
     while (true) {
         char command;
@@ -28,26 +34,55 @@ void ui(Register reg){
                 for (int i = 0; i < size(reg.emplVector); i++) {
                     cout << *(reg.emplVector[i]) << endl;
                 }
-            }
-                break;
+            }break;
             case 'e': {
                 string name;
                 cout << "name: ";
                 cin >> name;
                 cout << endl << *(reg.getByName(name));
-            }
-                break;
+            }break;
             case 'a': {
                 int l, u;
                 cout << "\nfrom: ";
                 cin >> l;
                 cout << "\nto: ";
                 cin >> u;
-                for (auto e: reg.getByAge(l, u)) {
-                    cout << endl << *e;
+                print(reg.getByAge(l, u));
+            }break;
+            case 's':{
+                set<string> sc;
+                cout<<"working days (3 letters format, q to end): "<<endl;
+                string day;
+                cin>>day;
+                while (day != "q" && sc.size()<7){
+                    sc.insert(day);
+                    cin>>day;
                 }
-            }
-                break;
+                for (auto e: reg.getByWorkingDays(sc)){
+                    cout<<*e;
+                }
+            }break;
+            case 'd':{
+                string dep;
+                cout<<"department: ";
+                cin>>dep;
+                print(reg.getByDepartment(dep));
+            }break;
+            case 'b':{
+                string boss;
+                cout<<"boss name: ";
+                cin>>boss;
+                print(reg.getDirectSubordinates(boss)); //
+            }break;
+            case  'i':{
+                string boss;
+                cout<<"boss name: ";
+                cin>>boss;
+                print(reg.getIndirectSubordinates(boss));
+            }break;
+            case 'c':{
+                reg.clear();
+            }break;
             case 'x':
                 return;
             default:
